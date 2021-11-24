d88f868d9c3651d3ce1827b21f439e14df319022
Alfina Azaria <alfinazaria32@gmail.com>
2021-11-24 17:03:11 +0700

    RU Profil Pengguna

--- pengguna/urls.py
+++ pengguna/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     path("penggalang_dana", form_pd, name="penggalang_dana"),
     path("penggalang_dana/individu", form_pd_individu, name="pd_individu"),
     path("penggalang_dana/organisasi", form_pd_organisasi, name="pd_organisasi"),
+    path("profil_pengguna", profil_pengguna, name="profil-pengguna"),
+    path("daftar-pengguna-admin", read_daftar_pengguna_admin, name="daftar-pengguna-admin"),
+    path("daftar-pengguna-admin-afterverif", read_daftar_pengguna_admin_afterverif, name="daftar-pengguna-admin-afterverif"),
+    path("profil-pengguna-individu-adminview", profil_pengguna_individu_adminview, name="profil-pengguna-individu-adminview"),
+    path("profil-pengguna-organisasi-adminview", profil_pengguna_organisasi_adminview, name="profil-pengguna-organisasi-adminview"),
+    
+
 
 ]
--- pengguna/views.py
+++ pengguna/views.py
@@ -11,3 +11,19 @@ def form_pd_individu(request):
 
 def form_pd_organisasi(request):
     return render(request, 'form_pd_organisasi.html')
+
+def profil_pengguna(request):
+    return render(request, 'profil_pengguna.html')
+
+# Admin only page. Pengguna cannot access
+def read_daftar_pengguna_admin(request):
+    return render(request, 'read_daftar_pengguna_admin.html')
+
+def read_daftar_pengguna_admin_afterverif(request):
+    return render(request, 'read_daftar_pengguna_admin_afterverif.html')
+
+def profil_pengguna_individu_adminview(request):
+    return render(request, 'profil_pengguna_individu_adminview.html')
+
+def profil_pengguna_organisasi_adminview(request):
+    return render(request, 'profil_pengguna_organisasi_adminview.html')
